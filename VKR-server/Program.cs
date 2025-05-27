c2c42ccb2f40518edde33f9e864ab79aa005b5e4
sergejnehorosev <sergo777ser777@gmail.com>
2025-05-27 08:02:37 +0300

    chore: remove redundant commentaries

--- VKR-server/Program.cs
+++ VKR-server/Program.cs
@@ -1,58 +1,48 @@
-using VKR_server.DB;
-using VKR_server.DB.Entities;
-using Microsoft.EntityFrameworkCore;
-using VKR_server.JWT;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using VKR_server.DB;
+using VKR_server.JWT;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options => //cors
+builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
+    options.AddPolicy("AllowAll", policyBuilder =>
     {
-        builder.WithOrigins("http://localhost:5173","http://localhost:4200")
+        policyBuilder.WithOrigins("http://localhost:5173", "http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
 });
 
-builder.Services.AddDbContext<ApplicationContext>(options => //context
+builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-//jwt
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters = new TokenValidationParameters
         {
-            ValidateIssuer = true, //валидация издателя
-            ValidIssuer = AuthOptions.ISSUER,
-
-            ValidateAudience = true, //валидация потребителя
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
             ValidAudience = AuthOptions.AUDIENCE,
-
-            ValidateLifetime = true, //валидация времени существования
-
-            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(), //получение ключа 
-             
-            ValidateIssuerSigningKey = true //валидация ключа безопасности
+            ValidIssuer = AuthOptions.ISSUER,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey()
         };
     });
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -68,4 +58,3 @@ app.MapControllers();
 app.UseCors("AllowAll");
 
 app.Run();
-//5288 7274
