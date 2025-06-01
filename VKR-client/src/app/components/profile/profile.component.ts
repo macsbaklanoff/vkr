8853659dacf6b2b6ccf3224cbfa7d78891a8db06
macsbaklanoff <baklanov689@gmail.com>
2025-06-01 18:04:09 +0300

    update profile

--- VKR-client/src/app/components/profile/profile.component.ts
+++ VKR-client/src/app/components/profile/profile.component.ts
@@ -12,6 +12,10 @@ import {SignOutDialogComponent} from '../dialogs/sign-out-dialog/sign-out-dialog
 import {UpdateUserDataComponent} from '../dialogs/update-user-data/update-user-data.component';
 import {EstimationService} from '../../services/estimation.service';
 import {IEstimationProfile} from '../../interfaces/estimation-profile-response';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {FileService} from '../../services/file.service';
+import {IInfoFileEstimationResponse} from '../../interfaces/info-file-estimation-response';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -23,7 +27,10 @@ import {IEstimationProfile} from '../../interfaces/estimation-profile-response';
     MatFormField,
     MatLabel,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatTabGroup,
+    MatTab,
+    NgStyle
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
@@ -32,8 +39,11 @@ export class ProfileComponent {
   private readonly _authService = inject(AuthService);
   private readonly _userService = inject(UserService);
   private readonly _estimationService = inject(EstimationService);
+  private readonly _fileService = inject(FileService);
 
   public authData = this._authService.authData();
+  public filesData: IInfoFileEstimationResponse[] = [];
+
   public estimationData: IEstimationProfile = {
     countWorks: 0,
     countRatedExc: 0,
@@ -57,6 +67,18 @@ export class ProfileComponent {
         this.estimationData = profile;
         },
     })
+    this._fileService.getFileEstimation(this.authData!.userId).subscribe({
+      next: (filesData: IInfoFileEstimationResponse[]) => {
+        this.filesData = filesData;
+      }
+    })
+  }
+
+  public getColor(estimation: number) : string {
+    if (estimation >= 81) return '#DB4242';
+    else if (estimation >= 61 && estimation < 81) return '#EBA134';
+    else if (estimation >=41 && estimation < 61) return '#dbd765';
+    return '#45a85b';
   }
 
   public changeStateEdit(): void {
